82cc084eba999f581d15e32283453907f0be6cf0
John Mcilroy <john_mcilroy@hotmail.co.uk>
2020-06-17 09:22:43 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,8 @@ function openModal() {
   
   // Get the image and insert it inside the modal - use its "alt" text as a caption
   const modalImg = document.getElementById("modal-img");
-  const captionText = document.getElementById("caption");
   
   modal.style.display = "block";
-  modalImg.src = event.target.src;
   captionText.innerHTML = event.target.alt;
   
   // When the user clicks on <span> (x), close the modal
